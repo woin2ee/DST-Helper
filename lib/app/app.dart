2ab99d06931158e355346ae7edf9e69b82c681ac
Jaewon Yun <woin2ee@gmail.com>
2024-08-20 21:23:29 +0900

    Add a language selection button

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,24 +1,39 @@
+import 'dart:io';
+
 import 'package:dst_helper/app/home_page.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  Locale _selectedLocale = Locale(Platform.localeName.split('_')[0]);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DST Helper',
       restorationScopeId: 'restoration_scope_id_dst_helper_0.0.1_0_debug',
-      locale: kDebugMode ? const Locale('ko') : null,
+      locale: _selectedLocale,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.brown.shade800),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: HomePage(
+        onSelectedLocale: (locale) {
+          setState(() {
+            _selectedLocale = locale;
+          });
+        },
+        selectedLocale: _selectedLocale,
+      ),
     );
   }
 }
--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -5,14 +5,27 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+enum AvailableLanguage {
+  en,
+  ko,
+}
+
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({
+    super.key,
+    required this.onSelectedLocale,
+    required this.selectedLocale,
+  });
+
+  final void Function(Locale) onSelectedLocale;
+  final Locale selectedLocale;
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  // Only debug use
   static const _firstPageIndex = kReleaseMode ? 0 : 0;
 
   final List<(Menu, bool)> _selectedMenuState = Menu.values.map((menu) {
@@ -26,18 +39,73 @@ class _HomePageState extends State<HomePage> {
 
     final selectedMenu = _selectedMenuState.firstWhere((element) => element.$2 == true).$1;
 
+    final languageButton = Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(4.0),
+        border: Border.all(width: 1.0, color: Colors.white),
+      ),
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(right: 8.0),
+            child: Icon(
+              Icons.language,
+              color: Colors.white,
+            ),
+          ),
+          Text(
+            widget.selectedLocale.localizedName,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 16.0,
+            ),
+          ),
+          const Icon(
+            Icons.arrow_drop_down,
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+
     return Scaffold(
       backgroundColor: colorScheme.surfaceContainerLowest,
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: Padding(
           padding: const EdgeInsets.only(right: 20),
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: Image.asset(
-              'assets/images/bg_loading_loading_charlie2.png',
-              height: 100,
-            ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                'assets/images/bg_loading_loading_charlie2.png',
+                height: 100,
+              ),
+              Row(
+                children: [
+                  PopupMenuButton<AvailableLanguage>(
+                    position: PopupMenuPosition.under,
+                    color: Colors.white,
+                    child: languageButton,
+                    onSelected: (AvailableLanguage item) {
+                      widget.onSelectedLocale(Locale(item.name));
+                    },
+                    itemBuilder: (BuildContext context) => [
+                      const PopupMenuItem(
+                        value: AvailableLanguage.en,
+                        child: Text('English'),
+                      ),
+                      const PopupMenuItem(
+                        value: AvailableLanguage.ko,
+                        child: Text('한국어'),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(width: 80),
+                ],
+              ),
+            ],
           ),
         ),
       ),
@@ -81,5 +149,18 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
+  }
+}
+
+extension on Locale {
+  String get localizedName {
+    switch (languageCode.split('_')[0]) {
+      case 'en':
+        return 'English';
+      case 'ko':
+        return '한국어';
+      default:
+        return 'None';
+    }
   }
 }
