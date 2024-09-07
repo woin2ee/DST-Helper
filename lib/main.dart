d087f5c227e951f3d8c5ca14c247bfdbcb253494
Jaewon Yun <woin2ee@gmail.com>
2024-09-08 01:00:48 +0900

    Configure managing data method

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,28 @@
 import 'package:dst_helper/app/app.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
-  // if (kDebugMode) {
-  //   WidgetsFlutterBinding.ensureInitialized();
-  //   final prefs = await SharedPreferencesWithCache.create(cacheOptions: const SharedPreferencesWithCacheOptions());
-  //   await prefs.clear();
-  // }
+  await _clearPrefsIfNeeds();
 
   FlutterError.onError = (details) {
     FlutterError.presentError(details);
   };
 
   runApp(const App());
+}
+
+Future<void> _clearPrefsIfNeeds() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferencesWithCache.create(cacheOptions: const SharedPreferencesWithCacheOptions());
+
+  // 버전 명시
+  const int currentVersion = 5;
+
+  final version = prefs.getInt('prefs_version');
+  if (version == null || version != currentVersion) {
+    await prefs.clear();
+    prefs.setInt('prefs_version', currentVersion);
+    return;
+  }
 }
