32151a574ef6ccc905bfde30d0b837f576d538d4
Jaewon Yun <woin2ee@gmail.com>
2025-02-06 17:18:51 +0900

    Resolve redering issues with preloading

--- lib/cook_page/cook_page.dart
+++ lib/cook_page/cook_page.dart
@@ -1,5 +1,6 @@
 import 'package:dst_helper/cook_page/recipe_card_table.dart';
 import 'package:dst_helper/cook_page/recipe_list/recipe_list.dart';
+import 'package:dst_helper/models/v2/item/item.dart';
 import 'package:flutter/material.dart';
 
 class CookPage extends StatefulWidget {
@@ -10,6 +11,12 @@ class CookPage extends StatefulWidget {
 }
 
 class _CookPageState extends State<CookPage> {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _prefetchImagesToNeed(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -23,6 +30,33 @@ class _CookPageState extends State<CookPage> {
           RecipeList(),
         ],
       ),
+    );
+  }
+
+  void _prefetchImagesToNeed(BuildContext context) {
+    precacheImage(const AssetImage('assets/images/frame_1.png'), context);
+    precacheImage(const AssetImage('assets/images/frame_2_thick.png'), context);
+
+    for (final ingredient in Items.ingredients) {
+      _prefetchIngredientImage(ingredient, context);
+    }
+
+    for (final category in FoodValueCategory.values) {
+      precacheImage(AssetImage('assets/images/items/${category.assetName}.png'), context);
+    }
+  }
+
+  void _prefetchIngredientImage(Item ingredient, BuildContext context) {
+    if (ingredient is CookableFood) {
+      precacheImage(
+        AssetImage('assets/images/items/${ingredient.compositeAssetName}.png'),
+        context,
+      );
+      return;
+    }
+    precacheImage(
+      AssetImage('assets/images/items/${ingredient.assetName}.png'),
+      context,
     );
   }
 }
