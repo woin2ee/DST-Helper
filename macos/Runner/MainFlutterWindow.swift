d3f93a61b8ec0fbfa88cce3e969f7b743c8690c7
Jaewon Yun <woin2ee@gmail.com>
2025-02-10 14:04:08 +0900

    Update icons and metadata for macos

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -5,8 +5,11 @@ class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
     let windowFrame = self.frame
+    let minimumSize = NSSize(width: 1200, height: 800)
+      
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
+    self.minSize = minimumSize
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
