13f0c75f2798ff87beaaa01f5528c16390c68cad
Jaewon Yun <woin2ee@gmail.com>
2025-02-13 16:24:10 +0900

    Refactor app state with provider

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,39 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:universal_io/io.dart';
+import 'package:provider/provider.dart';
 
 import '../l10n/l10ns.dart';
+import 'app_state.dart';
 import 'home_page.dart';
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  Locale _selectedLocale = Locale(Platform.localeName.split('_')[0]);
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'DST Helper',
-      restorationScopeId: 'restoration_scope_id_dst_helper_0.0.1_0_debug',
-      locale: _selectedLocale,
-      localizationsDelegates: L10ns.localizationsDelegates,
-      supportedLocales: L10ns.supportedLocales,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.brown.shade800),
-        useMaterial3: true,
-      ),
-      home: HomePage(
-        onSelectedLocale: (locale) {
-          setState(() {
-            _selectedLocale = locale;
-          });
-        },
-        selectedLocale: _selectedLocale,
-      ),
+    return ChangeNotifierProvider(
+      create: (context) => AppState(),
+      builder: (context, child) {
+        final appState = context.watch<AppState>();
+        return MaterialApp(
+          title: 'DST Helper',
+          restorationScopeId: 'restoration_scope_id_dst_helper_0.0.1_0_debug',
+          locale: appState.currentLocale,
+          localizationsDelegates: L10ns.localizationsDelegates,
+          supportedLocales: L10ns.supportedLocales,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.brown.shade800),
+            useMaterial3: true,
+          ),
+          home: const HomePage(),
+        );
+      },
     );
   }
 }
