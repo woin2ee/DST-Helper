08c7278deb772f7d0562d6d574912985ff7ae258
Jaewon Yun <woin2ee@gmail.com>
2025-02-23 23:58:13 +0900

    Fix tests

--- test/farm_page_test.dart
+++ test/farm_page_test.dart
@@ -1,24 +1,27 @@
 import 'package:dst_helper/farm_page/farm_page_model.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shared_preferences_platform_interface/in_memory_shared_preferences_async.dart';
 import 'package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart';
 
 void main() {
-  group('Test [FarmPageNotifier]', () {
+  group('Test model', () {
     late FarmPageModel subject;
 
     setUp(() {
       SharedPreferencesAsyncPlatform.instance = InMemorySharedPreferencesAsync.empty();
-      subject = FarmPageModel();
+      final prefs = SharedPreferencesWithCache.create(
+        cacheOptions: const SharedPreferencesWithCacheOptions(
+          allowList: {
+            FarmPageRepository.key,
+          },
+        ),
+      );
+      final repository = FarmPageRepository(prefs: Future.value(prefs));
+      subject = FarmPageModel(repository: repository);
     });
 
     test('Test initial state', () {
-      final initialList = subject.farmCardModelsBySelectedSeason;
-      expect(initialList.isEmpty, true);
-    });
-
-    test('Test `initFromPrefs` call', () async {
-      await subject.initFromPrefs();
       final initialList = subject.farmCardModelsBySelectedSeason;
       expect(initialList.isNotEmpty, true);
     });
